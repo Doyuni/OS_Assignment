8f9a4d54e5c3f175efab71316d4876d75ecdc0f6
Doyuni <gdypc@naver.com>
2019-03-11 13:00:01 +0900

    코드 주석 추가 및 정리

--- course_sched.c
+++ course_sched.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-// 선수과목이 없는 순, 알파벳이 빠른 순, 난이도 높은 순, 선수과목의 
 #define MAX_LENGTH 4096
 
 struct CourseEntry {
@@ -26,7 +25,7 @@ typedef struct Queue{
 }Queue;
 
 nodePointer graph[10];
-int id = -1;
+int id = -1; // index of graph
 
 Queue* init() {
 	Queue * q = (Queue*)malloc(sizeof(Queue));
@@ -38,20 +37,20 @@ Queue* init() {
 	return q;
 }
 int empty(const Queue *q) {
-	if (q->num == 0) return 1;
+	if (q->num == 0) return 1; // 비어있을 때
 	return 0;
 }
-void dequeue(Queue* q) {
+void dequeue(Queue* q) { // 삭제
 	if (empty(q)) return;
 	q->num--;
 	q->que[q->front++] = -1;
 }
-void enqueue(Queue* q, int value) {
+void enqueue(Queue* q, int value) { // 삽입
 	if (q->num == q->max) return;
 	q->num++;
 	q->que[q->rear++] = value;
 }
-int front(Queue* q) {
+int front(Queue* q) { // 큐 맨 앞의 자료 추출
 	if (empty(q)) return;
 	return q->que[q->front];
 }
@@ -59,7 +58,7 @@ nodePointer createNode(int v, char *name) {
 	nodePointer newNode = (nodePointer*)malloc(sizeof(node));
 	strcpy(newNode->lecture.name, name);
 	newNode->lecture.prerequisites = NULL;
-	newNode->lecture.difficulty = 5;
+	newNode->lecture.difficulty = 5; // 난이도 기본값 5
 	newNode->lecture.n_prerequisites = 0;
 	newNode->vertex = v;
 	return newNode;
@@ -72,29 +71,30 @@ int nameToInt(char *name) {
 		if (graph[i] != NULL) {
 			if (strcmp(graph[i]->lecture.name, name) == 0) {
 				id--;
-				return i; //이름과 상응하는 인접리스트 인덱스 리턴
+				return i; // 이름과 상응하는 노드 인덱스 리턴
 			}
 		}
 	}
 	return id;
 }
+// 노드 교환
 void swap(int i, int j) {
 	nodePointer temp;
 	temp = graph[i];
 	graph[i] = graph[j];
 	graph[j] = temp;
 }
-
+// 난이도, 알파벳 순으로 정렬 (단, 정렬 우선순위는 난이도가 높다.)
 void sort() {
-	for (int i = 0; i < 3; ++i) {
-		for (int j = i+1; j < 3; ++j)
+	for (int i = 0; i < id; ++i) {
+		for (int j = i+1; j < id; ++j)
 		{	
 			float a = graph[i]->lecture.difficulty;
 			float b = graph[j]->lecture.difficulty;
 			if (a < b) {
 				swap(i, j);
 			}
-			else {
+			else { 
 				char *tmp = graph[i]->lecture.name;
 				char *tmp2 = graph[j]->lecture.name;
 				if (strcmp(tmp, tmp2) > 0) {
@@ -102,15 +102,14 @@ void sort() {
 				}
 			}
 		}
-		
 	}
 }
 int main(int argc, char** argv)
 {
-	int num = 0;
-	char *token = NULL;
-	char line[256];
-	char temp[30];
+	int num = 0; // node 개수
+	char *token = NULL; // 자른 문자열
+	char line[256]; // 문자열 한 줄 읽기
+	char temp[30];  
 
 	FILE* fp = fopen("./in/database.csv", "r");
 	if (fp == NULL) {
@@ -120,7 +119,6 @@ int main(int argc, char** argv)
 	while (fgets(line, sizeof(line), fp)) {
 		int cnt = 0;
 		token = strtok(line, ",");
-		
 		while (token != NULL) {
 			if (token[0] == ' ') {
 				int len = strlen(token);
@@ -130,7 +128,6 @@ int main(int argc, char** argv)
 				}
 				temp[len - 1] = '\0';
 			}
-			
 			// 선수 과목 수
 			if (token[0] >= '0' && token[0] <= '9') {
 				graph[num]->lecture.n_prerequisites = token[0] - '0';
@@ -141,15 +138,14 @@ int main(int argc, char** argv)
 			else { // 과목이름
 				graph[num] = createNode(nameToInt(token), token);
 			}
- 			if(cnt < 1) token = strtok(NULL, ", \n");
-			else if (cnt >= 1) token = strtok(NULL, ",\n");
+ 			if(cnt < 1) token = strtok(NULL, ", \n"); // 선수 과목 수까지 파싱할 때
+			else if (cnt >= 1) token = strtok(NULL, ",\n"); // 선수 과목들 이름 파싱할 때
 			++cnt;
 		}
 		++num;
 	}
 	if (fp != NULL) fclose(fp);
-
-	// 난이도 입력 부분(명령어 인자로 받아야 함)
+	// 난이도 정보 넣기 
 	for (int i = 0; i < argc; ++i) {
 		FILE* fin = fopen(argv[i], "r");
 		if (fin == NULL) {
@@ -172,9 +168,9 @@ int main(int argc, char** argv)
 		}
 		if (fin != NULL) fclose(fin);
 	}
-	// alphabet, difficulty sort
+	// 난이도 순, 알파벳 순으로 정렬
 	sort();
-	// topology sort
+	// topological sorting (위상 정렬)
 	Queue* q = init();
 	for (int i = 0; i < num; ++i) {
 		if (!graph[i]->lecture.n_prerequisites) enqueue(q, i);
