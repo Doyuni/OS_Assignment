d13bef0c59dcf0b130e8ce39b025854c383b3e6c
Doyuni <gdypc@naver.com>
2019-06-02 01:06:33 +0900

    final parallel : tokenizing multithreading, hashTable and quickSort / add resultImage

--- PA2/src/wc.c
+++ PA2/src/wc.c
@@ -1,64 +1,86 @@
+#define _GNU_SOURCE // for strtok_r 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <sys/queue.h>
 #include <pthread.h>
-#include <sys/time.h>
-// sys queue 부분
-static LIST_HEAD(listhead, entry) head; // singly linked list (tail이 null로 끝남)
-
-struct listhead *headp = NULL;
-// 여기까지 head define
-
-int num_entries = 0;
-
-struct entry
+// #include <sys/time.h>
+#define HASHMAX 210000000
+typedef struct HashNode
 {
-  char name[BUFSIZ];
+  char *word;
   int frequency;
-  LIST_ENTRY(entry)
-  entries; // link를 의미 entries 구조체
-};
+  struct HashNode *link;
+} HashNode;
 
-//
-static char *buf;
-static int buf_len = 0;
-
-static char *buf_for_parallel;
-
-static char buf2_for_parallel[4096] = {
+int global_count = 0;
+HashNode *HashTable[HASHMAX] = {
     0,
 };
-const int k_num_thread = 4;
-static int counter = 0;
-static pthread_mutex_t counter_mutex = PTHREAD_MUTEX_INITIALIZER;
+static char *buf;
+
+const int num_thread = 4;
+static pthread_mutex_t insert_mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t visitor_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 int file_size = 0;
 int portion_size = 0;
-int extra_size = 0;
+
 FILE *fp;
+const char *WHITE_SPACE = " \t\n";
+static char *tok;
+char *last;
+
+unsigned int HashFunc(const char *word) // 해시 함수
+{
+  unsigned int index = 0;
+  int n = strlen(word);
+  for (int i = 0; i < n; i++) {
+    index = 31 * index + word[i]; // 31(소수)을 곱하여 가장 바르게 계산 가능
+  }
+  return index % HASHMAX;
+}
+
+void InsertWord(const char *word)
+{
+  HashNode *ptr;
+  int index = HashFunc(word);
+  for (ptr = HashTable[index]; ptr != NULL; ptr = ptr->link)
+  { 
+    if (!strcmp(word, ptr->word))
+    {
+      (ptr->frequency)++;
+      return;
+    }
+  }
+  global_count++;
+  ptr = (HashNode *)malloc(sizeof(HashNode)); 
+  ptr->frequency = 1;
+  ptr->word = (char *)malloc(strlen(word) + 1);
+  strcpy(ptr->word, word);
+  ptr->link = HashTable[index];
+  HashTable[index] = ptr;
+}
 
 void *parallel_str_transform(void *arg)
 {
-  int portion_size, start, end;
-  int tid = *(int *)(arg); // get the thread ID assigned sequentially.
+  int start, end;
+  int tid = *(int *)(arg); // thread id
   start = tid * portion_size;
   end = (tid + 1) * portion_size;
-  // pthread_mutex_lock(&counter_mutex);
-  // pthread_mutex_unlock(&counter_mutex);
+  // matrix 곱셈 병렬화 응용
   for (int i = start; i < end; ++i)
   {
     if (!isalnum(buf[i]))
     {
-      buf_for_parallel[i] = ' ';
+      buf[i] = ' ';
     }
     else
     {
-      buf_for_parallel[i] = tolower(buf[i]);
+      buf[i] = tolower(buf[i]);
     }
   }
-  if (tid == k_num_thread - 1)
+  if (tid == num_thread - 1)
   { // 나머지 처리 (스레드 개수랑 나누어 떨어지지 않을 때)
     start = end;
     end = file_size;
@@ -66,61 +88,109 @@ void *parallel_str_transform(void *arg)
     {
       if (!isalnum(buf[i]))
       {
-        buf_for_parallel[i] = ' ';
+        buf[i] = ' ';
       }
       else
       {
-        buf_for_parallel[i] = tolower(buf[i]);
+        buf[i] = tolower(buf[i]);
       }
     }
   }
   pthread_exit(0);
 }
 
-// void *parallel_str_token(void *changed_buf)
-// {
-//   strcpy(buf2_for_parallel, (char *)changed_buf);
-
-//   pthread_mutex_lock(&counter_mutex);
-//   int id = counter++;
-//   pthread_mutex_unlock(&counter_mutex);
-
-//   tok = strtok(buf2_for_parallel, WHITE_SPACE);
-//   do
-//   {
-//     if (tok == NULL || strcmp(tok, "") == 0)
-//       continue;
-//     printf("thread %d tok : %s\n", id, tok);
-//   } while (tok = strtok(NULL, WHITE_SPACE));
-// }
-
-int main()
+void *parallel_str_token(void *arg)
 {
-  fp = fopen("./data/therepublic.txt", "r");
-
-  if (!fp)
+  int tid = *(int *)(arg); // thread id : parallel test용
+  do
   {
-    fprintf(stderr, "Failed to open ./data/strings.txt\n");
+    pthread_mutex_lock(&visitor_mutex); // 방문 락
+    tok = strtok_r(NULL, WHITE_SPACE, &last);
+    if (tok)
+    {
+      // printf("thread %d : %s\n",tid, tok); // test용
+      pthread_mutex_lock(&insert_mutex); // word 삽입 락
+      if(isalnum(tok[0])) { // owl.txt에 오류나서 추가했습니다. - 오류해결
+        InsertWord(tok); // hashtable에 추가
+      }
+      pthread_mutex_unlock(&insert_mutex); // word 락 해제
+    }
+    pthread_mutex_unlock(&visitor_mutex); // 방문 락 해제
+  } while (tok);
+  pthread_exit(0);
+}
+
+static int compareWordCount(const void *left, const void *right)
+{ // 빈도수 비교
+  int cmp = (*(const HashNode **)right)->frequency - (*(const HashNode **)left)->frequency;
+  if (cmp == 0) // 빈도수 같을 때
+  { // 사전 순 비교
+    cmp = strcmp((*(const HashNode **)right)->word, (*(const HashNode **)left)->word);
+    cmp = cmp * -1; // 알파벳 순서
+  }
+  return cmp;
+}
+
+void printWord()
+{
+  HashNode **list = NULL;
+  unsigned int i = 0;
+  unsigned int index = 0;
+
+  list = malloc(global_count * sizeof(HashNode *));
+  // list에 node 연결
+  for (i = 0, index = 0; i < HASHMAX; ++i)
+  {
+    HashNode *ptr = HashTable[i];
+    while (ptr)
+    {
+      list[index++] = ptr;
+      ptr = ptr->link;
+    }
+  }
+
+  // quick sort, compareWordCount 기준으로 list 정렬
+  qsort(list, index, sizeof(HashNode *), compareWordCount);
+
+  for (i = 0; i < index; i++) {
+    printf("%s %d\n", list[i]->word, list[i]->frequency);
+  }
+  free(list);
+}
+
+int main(int argc, char **argv)
+{
+  // struct timeval tstart, tend;
+  // double exectime;
+  // gettimeofday(&tstart, NULL); // start time
+  if (argc != 2)
+  {
+    fprintf(stderr, "%s: not enough input\n", argv[0]);
     exit(1);
   }
-  struct timeval tstart, tend;
-  double exectime;
+
+  fp = fopen(argv[1], "r");
+  if (!fp)
+  {
+    fprintf(stderr, "Failed to open %s\n", argv[1]);
+    exit(1);
+  }
 
   fseek(fp, 0, SEEK_END);
-  file_size = ftell(fp);
-  portion_size = file_size / k_num_thread;
-  extra_size = file_size % k_num_thread;
-  // printf("file size : %d\n", file_size);
-  buf_for_parallel = malloc(sizeof(char) * file_size);
-  buf = malloc(sizeof(char) * file_size);
-  fseek(fp, 0, SEEK_SET);
+  file_size = ftell(fp); // 전체 파일 크기
+  // printf("file size : %d\n", file_size); // for test
+  fseek(fp, 0, SEEK_SET); // 파일 포인터 처음으로 설정
 
-  fread(buf, file_size, 1, fp);
-  // Parallel
-  gettimeofday( &tstart, NULL );
-  pthread_t pid[k_num_thread];
-  
-  for (int i = 0; i < k_num_thread; ++i)
+  portion_size = file_size / num_thread;
+
+  buf = malloc(sizeof(char) * file_size);
+  last = malloc(sizeof(char) * file_size); // for strtok_r 
+  fread(buf, file_size, 1, fp); // 전체 파일 읽기
+ 
+  pthread_t pid[num_thread];
+
+  // preprocessing
+  for (int i = 0; i < num_thread; ++i)
   {
     int *tid;
     tid = (int *)malloc(sizeof(int));
@@ -128,178 +198,36 @@ int main()
     pthread_create(&pid[i], NULL, parallel_str_transform, (void *)tid);
   }
 
-  for (int i = 0; i < k_num_thread; ++i)
+  for (int i = 0; i < num_thread; ++i)
   {
     pthread_join(pid[i], NULL);
   }
 
-  // for (int i = 0; i < k_num_thread; ++i)
-  // {
-  //   pthread_create(&pid[i], NULL, parallel_str_token, (void*)buf_for_parallel);
-  // }
-
-  // for (int i = 0; i < k_num_thread; ++i)
-  // {
-  //   pthread_join(pid[i], NULL);
-  // }
-
-  // printf("%s\n", buf_for_parallel);
-
-  LIST_INIT(&head);
-
-  const char *WHITE_SPACE = " \t\n";
-  char* tok = strtok(buf_for_parallel, WHITE_SPACE);
-
-  //token
-
-  do
+  // tokenizing
+  tok = strtok_r(buf, WHITE_SPACE, &last);
+  InsertWord(tok); // 첫 번째 단어 삽입
+  for (int i = 0; i < num_thread; ++i)
   {
-    // 이걸 더 깔끔하게 바꿀 수 있을 것임
-    	if (tok == NULL || strcmp(tok, "") == 0) {
-			continue;
-		}
-
-    if (num_entries == 0)
-    {
-      struct entry *e = malloc(sizeof(struct entry));
-
-      strncpy(e->name, tok, strlen(tok)); // copy string
-      e->frequency = 1;
-
-      LIST_INSERT_HEAD(&head, e, entries);
-      num_entries++;
-      continue;
-    }
-    else if (num_entries == 1)
-    {
-      int cmp = strcmp(tok, head.lh_first->name);
-
-      if (cmp == 0)
-      {
-        head.lh_first->frequency++;
-      }
-      else if (cmp > 0)
-      {
-        struct entry *e = malloc(sizeof(struct entry));
-
-        strncpy(e->name, tok, strlen(tok));
-        e->frequency = 1;
-
-        LIST_INSERT_AFTER(head.lh_first, e, entries);
-        num_entries++;
-      }
-      else if (cmp < 0)
-      {
-        struct entry *e = malloc(sizeof(struct entry));
-
-        strncpy(e->name, tok, strlen(tok));
-        e->frequency = 1;
-
-        LIST_INSERT_HEAD(&head, e, entries);
-        num_entries++;
-      }
-
-      continue;
-    }
-
-    // Reduce: actual word-counting
-    struct entry *np = head.lh_first;
-    struct entry *final_np = NULL;
-
-    int last_cmp = strcmp(tok, np->name); // 삽입정렬을 사용하였기에 아래 부분(앞에가 정렬되어 있다는 전제)
-
-    if (last_cmp < 0)
-    { // tok이 name의 이전에 와야한다.
-      struct entry *e = malloc(sizeof(struct entry));
-
-      strncpy(e->name, tok, strlen(tok));
-      e->frequency = 1;
-
-      LIST_INSERT_HEAD(&head, e, entries);
-      num_entries++;
-
-      continue;
-    }
-    else if (last_cmp == 0)
-    {
-      np->frequency++;
-
-      continue;
-    }
-    // 삽입 정렬 -> 병렬화를 했을 때 가능한지 봐야함
-    for (np = np->entries.le_next; np != NULL; np = np->entries.le_next)
-    {
-      int cmp = strcmp(tok, np->name);
-
-      if (cmp == 0)
-      {
-        np->frequency++;
-        break;
-      }
-      else if (last_cmp * cmp < 0)
-      { // sign-crossing occurred,  A B D E 에 C를 넣고 싶을 때  cmp하면 1 1 -1 에서 1 * -1이면 즉 부호가 바뀜
-        struct entry *e = malloc(sizeof(struct entry));
-
-        strncpy(e->name, tok, strlen(tok));
-        e->frequency = 1;
-
-        LIST_INSERT_BEFORE(np, e, entries);
-        num_entries++;
-
-        break;
-      }
-
-      if (np->entries.le_next == NULL)
-      {
-        final_np = np;
-      }
-      else
-      {
-        last_cmp = cmp;
-      }
-    }
-
-    if (!np && final_np)
-    { // 맨 마지막에 넣어줌, 마지막 tail이 NULL이기에 prev를 찾을 방법이 필요
-      struct entry *e = malloc(sizeof(struct entry));
-
-      strncpy(e->name, tok, strlen(tok));
-      e->frequency = 1;
-
-      LIST_INSERT_AFTER(final_np, e, entries);
-      num_entries++;
-    }
-  } while (tok = strtok(NULL, WHITE_SPACE));
-
-  int max_frequency = 0;
-
-  for (struct entry* np = head.lh_first; np != NULL; np = np->entries.le_next) {
-    if (max_frequency < np->frequency) {
-      max_frequency = np->frequency;
-    }
+    int *tid;
+    tid = (int *)malloc(sizeof(int));
+    *tid = i;
+    pthread_create(&pid[i], NULL, parallel_str_token, (void *)tid);
   }
-  // print할 수 있는 방법은 많다. qsort 함수 쓰는게 좋을 수 있음 하지만 unstable sort이기에 잘 생각해야함
-  // stable sort: D E A-34 A-4 B를 알파벳 순서대로 정렬 A-34 A-4 B D E를 보장, unstable sort는 보장 안함
-  // red-black tree도 가능
-  for (int it = max_frequency; it > 0; --it) {
-    for (struct entry* np = head.lh_first; np != NULL; np = np->entries.le_next) {
-      if (np->frequency == it) {
-        printf("%s %d\n", np->name, np->frequency);
-      }
-    }
+
+  for (int i = 0; i < num_thread; ++i)
+  {
+    pthread_join(pid[i], NULL);
   }
-  gettimeofday( &tend, NULL );
-  exectime = (tend.tv_sec - tstart.tv_sec) * 1000.0; // sec to ms
-  exectime += (tend.tv_usec - tstart.tv_usec) / 1000.0; // us to ms   
-  printf( "Number of threads: %d\tExecution time:%.3lf sec\n",
-          k_num_thread, exectime/1000.0);
-  // Release
-  while (head.lh_first != NULL) {
-    LIST_REMOVE(head.lh_first, entries);
-  }
-  
+
+  // print
+  printWord();
+
+  // gettimeofday(&tend, NULL); // end time
+  // exectime = (tend.tv_sec - tstart.tv_sec) * 1000.0;    // sec to ms
+  // exectime += (tend.tv_usec - tstart.tv_usec) / 1000.0; // us to ms
+  // printf("Execution time:%.3lf sec\n", exectime / 1000.0);
+
   fclose(fp);
 
-  return 0;
-}
-
+    return 0;
+  }
